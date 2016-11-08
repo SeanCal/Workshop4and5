96958f8e652d91ea2eabb354ed1b18a94ebfb990
SeanCal <sgcallahan@umass.edu>
2016-11-08 18:56:46 -0500

    fb5

--- app/components/feed.js
+++ app/components/feed.js
@@ -1,14 +1,46 @@
 import React from 'react';
 import FeedItem from './feeditem';
 import StatusUpdateEntry from './statusupdateentry';
+import {getFeedData} from '../server';
 
 export default class Feed extends React.Component {
-  render() {
+  constructor(props) {
+    //super() calls the parent class constructor --
+    //e.g. React.Component's constructor.
+    super(props);
+    //Set state's initial value.
+    //note that the constructor is the only place
+    //you should ever set state directly!
+    // in all other places, use the 'setState' method instead.
+    //setting state directly in other places will not trigget render()
+    // to run so your program will have bugs.
+    this.state = {
+      //empty feed
+      contents: []
+    };
+  }
+
+  render(){
     return (
       <div>
         <StatusUpdateEntry />
-        <FeedItem />
+          {this.state.contents.map((feedItem) => {
+            return (
+              <FeedItem key={feedItem._id} data={feedItem} />
+            );
+          })}
       </div>
     )
+  }
+
+  componentDidMount() {
+    getFeedData(this.props.user, (feedData) => {
+      // Note: setState does a *shallow merge* of
+      // the current state and the new state. If
+      // state was currently set to {foo: 3}, and
+      // we setState({bar: 5}), state would then be
+      // {foo: 3, bar: 5}. This won't be a problem here.
+      this.setState(feedData);
+    });
   }
 }
